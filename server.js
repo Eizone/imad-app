b2dc97ecdc6b620d3457eba17e570d87aaab7147
Eizone <sriharivasu@gmail.com>
2017-08-08 18:23:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("Article one requiested");
+  res.sendFile();
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("Article two requiested");
+  res.sendFile();
 });
 
 app.get('/article-three', function (req, res) {
-  res.send("Article three requiested");
+  res.sendFile();
 });
 
 app.get('/ui/style.css', function (req, res) {
